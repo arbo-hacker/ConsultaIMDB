595294c729adfeac8cd23589e11c129200a6a0dc
Alejandro Barreto <arbo.com.ve@gmail.com>
2015-12-01 18:00:31 -0430

    Se versión aplicación de pruebas para leer los datos básicos de una película de IMDB a través de su código apoyándose en un servicio JSON de la pagina www.omdbapi.com y en la librería Alamofire

---
+++ ConsultaIMDB/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//import Alamofire
+
+
+var str = "Hello, playground"
+/*
+Alamofire.request(.GET, "http://www.omdbapi.com/?i=tt1520211&plot=short&r=json").responseJSON() {
+    (_, _, JSON, error) in
+    if error == nil {
+        let info =  JSON as NSDictionary
+        self.fecha.text = info["date"] as? String
+    } else {
+        println(error)
+    }
+}
+*/
--- ConsultaIMDB/ViewController.swift
+++ ConsultaIMDB/ViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imdbCode: UITextField!
+    @IBOutlet weak var titulo: UILabel!
+    @IBOutlet weak var año: UILabel!
+    @IBOutlet weak var duracion: UILabel!
+    @IBOutlet weak var genero: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //obtieneTitulo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func consultarPrecio(sender: AnyObject) {
+        let urls = "http://www.omdbapi.com/?i=\(imdbCode.text!)&plot=short&r=json"
+        print (urls)
+        Alamofire.request(.GET, urls).responseJSON
+            { respuesta in
+                
+                if respuesta.result.isSuccess{
+                    if let data = respuesta.result.value{
+                        if  data["Response"]!!.description != "False"{
+                            self.titulo.text = data["Title"]!!.description
+                            self.año.text = String( data["Year"]!!.description)
+                            self.duracion.text = String( data["Runtime"]!!.description)
+                            self.genero.text = String( data["Genre"]!!.description)
+                        }else{
+                            let alertController = UIAlertController(title: "iOScreator", message:
+                                "Debe introducir un codigo valido", preferredStyle: UIAlertControllerStyle.Alert)
+                            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+                            
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                        }
+                    }
+                }
+        }
+        
     }
 
 
